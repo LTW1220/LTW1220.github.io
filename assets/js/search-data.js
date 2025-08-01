1488cca6126ebf76883a23645c3af348905d32db
LTW1220 <120274003+LTW1220@users.noreply.github.com>
2025-08-01 17:58:18 +0000

    Deploying to gh-pages from @ LTW1220/LTW1220.github.io@ebe34db1e2eaf05e8f59c86c464d360395ba1261 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -396,7 +396,7 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-targeting-neuroinflammation-by-bioreactive-nanoparticle-for-early-detection-and-intervention-of-alzheimer-s-disease",
-          title: 'Targeting Neuroinflammation by Bioreactive Nanoparticle for Early Detection and Intervention of Alzheimer’s Disease...',
+          title: 'Targeting neuroinflammation by bioreactive nanoparticle for early detection and intervention of Alzheimer’s Disease...',
           description: "A blood–brain barrier (BBB)-crossing manganese dioxide nanoparticle enables early detection through MRI signal enhancement and disease modification by reducing pathological factors such as ROS, hypoxia, and phosphorylated tau in the brains of Alzheimer’s disease (AD) mouse models.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
